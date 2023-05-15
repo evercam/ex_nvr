d1823799b8ddc3c7feee417e839f3e983bab5086
Billal GHILAS <bilal@evercam.io>
2023-05-15 20:45:56 +0100

    Add tests

--- apps/ex_nvr_web/test/ex_nvr_web/controllers/error_json_test.exs
+++ apps/ex_nvr_web/test/ex_nvr_web/controllers/error_json_test.exs
@@ -2,7 +2,7 @@ defmodule ExNVRWeb.ErrorJSONTest do
   use ExNVRWeb.ConnCase, async: true
 
   test "renders 404" do
-    assert ExNVRWeb.ErrorJSON.render("404.json", %{}) == %{errors: %{detail: "Not Found"}}
+    assert ExNVRWeb.ErrorJSON.render("404.json", %{}) == %{message: "not found"}
   end
 
   test "renders 500" do
