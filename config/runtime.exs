b145b078c51ae1ad814f4e539c78e690d69ff6d1
Billal GHILAS <bilal@evercam.io>
2023-05-26 17:50:28 +0100

    Add docker file

--- config/runtime.exs
+++ config/runtime.exs
@@ -1,11 +1,5 @@
 import Config
 
-# config/runtime.exs is executed for all environments, including
-# during releases. It is executed after compilation and before the
-# system starts, so it is typically used to load production configuration
-# and secrets from environment variables or elsewhere. Do not define
-# any compile-time configuration in here, as it won't be applied.
-# The block below contains prod specific runtime configuration.
 if config_env() == :prod do
   database_path =
     System.get_env("DATABASE_PATH") ||
@@ -38,22 +32,12 @@ if config_env() == :prod do
 
   config :ex_nvr_web, ExNVRWeb.Endpoint,
     http: [
-      # Enable IPv6 and bind on all interfaces.
-      # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
       ip: {0, 0, 0, 0, 0, 0, 0, 0},
       port: String.to_integer(System.get_env("PORT") || "4000")
     ],
     secret_key_base: secret_key_base
 
-  # ## Using releases
-  #
-  # If you are doing OTP releases, you need to instruct Phoenix
-  # to start each relevant endpoint:
-  #
-  #     config :ex_nvr_web, ExNVRWeb.Endpoint, server: true
-  #
-  # Then you can assemble a release by calling `mix release`.
-  # See `mix help release` for more information.
+  config :ex_nvr_web, ExNVRWeb.Endpoint, server: true
 
   # ## SSL Support
   #
