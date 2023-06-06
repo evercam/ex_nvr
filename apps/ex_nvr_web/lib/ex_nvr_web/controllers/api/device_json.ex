0c1e797ca29a8f360b33ff3a0297f6c2c78564d0
Billal GHILAS <bilal@evercam.io>
2023-06-06 08:51:20 +0100

    Add created at field to device json

--- apps/ex_nvr_web/lib/ex_nvr_web/controllers/api/device_json.ex
+++ apps/ex_nvr_web/lib/ex_nvr_web/controllers/api/device_json.ex
@@ -3,7 +3,7 @@ defmodule ExNVRWeb.API.DeviceJSON do
 
   def show(%{device: device}) do
     device
-    |> Map.take([:id, :name, :type])
+    |> Map.take([:id, :name, :type, :created_at])
     |> Map.merge(Map.from_struct(device.ip_camera_config))
   end
 end
