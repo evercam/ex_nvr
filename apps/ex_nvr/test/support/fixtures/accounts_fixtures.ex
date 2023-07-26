e9962d15ae4e18a247b3a63436cdee713464c07d
Mebareksaf <mebareksafari8@gmail.com>
2023-07-26 12:57:44 +0100

    add new fields in accounts fixtures

--- apps/ex_nvr/test/support/fixtures/accounts_fixtures.ex
+++ apps/ex_nvr/test/support/fixtures/accounts_fixtures.ex
@@ -6,11 +6,16 @@ defmodule ExNVR.AccountsFixtures do
 
   def unique_user_email, do: "user#{System.unique_integer()}@example.com"
   def valid_user_password, do: "Hello world!"
+  def valid_first_name, do: "test_first_name#{:rand.uniform(999999)}"
+  def valid_last_name, do: "test_last_name#{:rand.uniform(999999)}"
+  def valid_username, do: "test_username#{:rand.uniform(999999)}"
 
   def valid_user_attributes(attrs \\ %{}) do
     Enum.into(attrs, %{
       email: unique_user_email(),
-      password: valid_user_password()
+      password: valid_user_password(),
+      first_name: valid_first_name(),
+      last_name: valid_last_name()
     })
   end
 
