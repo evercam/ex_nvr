ffedba24c40e288aa4508e96f4bef01cddedd1ab
Mebareksaf <mebareksafari8@gmail.com>
2023-08-09 13:35:43 +0100

    delete unnecessary test

--- apps/ex_nvr/test/ex_nvr/token_pruner_test.exs
+++ apps/ex_nvr/test/ex_nvr/token_pruner_test.exs
@@ -6,7 +6,6 @@ defmodule ExNVR.TokenPrunerTest do
 
   alias ExNVR.Accounts
   alias ExNVR.Accounts.UserToken
-  alias ExNVR.TokenPruner
 
   describe "Run delete_expired_tokens job" do
     setup do
@@ -24,22 +23,6 @@ defmodule ExNVR.TokenPrunerTest do
         valid_access: valid_access,
         valid_session: valid_session
       }
-    end
-
-    test "expired_tokens/1 returns the correct rows", %{
-      expired_access: expired_access,
-      expired_session: expired_session
-    } do
-      all_expired_tokens_query = UserToken.get_expired_tokens()
-
-      count_rows =
-        from(all_expired_tokens_query, select: count())
-        |> Repo.one()
-
-      assert count_rows == 2
-
-      assert Repo.exists?(from t in all_expired_tokens_query, where: t.id == ^expired_session.id)
-      assert Repo.exists?(from t in all_expired_tokens_query, where: t.id == ^expired_access.id)
     end
 
     test "delete_all_expired_tokens/0 deletes all expired tokens.", %{
