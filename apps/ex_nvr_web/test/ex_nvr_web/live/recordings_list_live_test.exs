d55eb9fe7aa3cca8d6b42d23a086a76114fcbc6d
Mebareksaf <mebareksafari8@gmail.com>
2023-08-14 17:00:44 +0100

    adds generating more recordings on test setup and testing pagination elemnts work

--- apps/ex_nvr_web/test/ex_nvr_web/live/recordings_list_live_test.exs
+++ apps/ex_nvr_web/test/ex_nvr_web/live/recordings_list_live_test.exs
@@ -10,20 +10,45 @@ defmodule ExNVRWeb.RecordingListLiveTest do
 
   describe "Recording list page" do
     setup %{device: device} do
-      %{recording: recording_fixture(device)}
+      %{
+        recordings: Enum.map(1..500, fn _ -> recording_fixture(device) end)
+      }
     end
 
-    test "render recordings page", %{conn: conn, device: device, recording: recording} do
-      {:ok, _lv, html} =
+    test "render recordings page", %{conn: conn, device: device, recordings: recordings} do
+      {:ok, lv, html} =
         conn
         |> log_in_user(user_fixture())
         |> live(~p"/recordings")
 
-      assert html =~ "#{recording.id}"
-      assert html =~ device.name
+      assert lv
+              |> element("a", "Previous")
+              |> has_element?()
+      assert lv
+              |> element("a", "Next")
+              |> has_element?()
+      assert lv
+              |> element("a", "1")
+              |> has_element?()
+      assert lv
+              |> element("a", "2")
+              |> has_element?()
+      assert lv
+              |> element("a", "3")
+              |> has_element?()
+      assert lv
+              |> element("a", "4")
+              |> has_element?()
+      assert lv
+              |> element("a", "5")
+              |> has_element?()
+      # for recording <- recordings do
+      #   assert html =~ "#{recording.id}"
+      #   assert html =~ device.name
 
-      assert html =~ "#{DateTime.to_iso8601(recording.start_date, :extended, 0)}"
-      assert html =~ "#{DateTime.to_iso8601(recording.end_date, :extended, 0)}"
+      #   assert html =~ "#{DateTime.to_iso8601(recording.start_date, :extended, 0)}"
+      #   assert html =~ "#{DateTime.to_iso8601(recording.end_date, :extended, 0)}"
+      # end
     end
   end
 end
