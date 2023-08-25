416371929f520eee82c22d51b5f05a8aa00ceb0b
Mebareksaf <mebareksafari8@gmail.com>
2023-08-25 14:56:27 +0100

    make sorting icon bigger

--- apps/ex_nvr_web/lib/ex_nvr_web/components/flop_config.ex
+++ apps/ex_nvr_web/lib/ex_nvr_web/components/flop_config.ex
@@ -7,11 +7,13 @@ defmodule ExNVRWeb.FlopConfig do
       thead_attrs: [
         class: "text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400"
       ],
-      thead_th_attrs: [class: "px-6 py-3 relative p-0 pb-4"],
+      thead_th_attrs: [class: "px-6 py-3 relative p-0 pb-2"],
       tbody_tr_attrs: [
         class: "bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50"
       ],
-      tbody_td_attrs: [class: "relative w-14 p-0 p-4"]
+      tbody_td_attrs: [class: "relative w-14 p-0 p-4"],
+      symbol_attrs: [class: "text-xl"],
+
     ]
   end
 end
