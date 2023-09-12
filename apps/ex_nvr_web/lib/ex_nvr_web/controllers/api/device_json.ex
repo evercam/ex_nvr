aa98282f6613433cf2ca86a9ae350a5ae0b1433a
Mebareksaf <mebareksafari8@gmail.com>
2023-09-12 16:09:25 +0100

    adapt changes in the devices endpoint

--- apps/ex_nvr_web/lib/ex_nvr_web/controllers/api/device_json.ex
+++ apps/ex_nvr_web/lib/ex_nvr_web/controllers/api/device_json.ex
@@ -6,6 +6,5 @@ defmodule ExNVRWeb.API.DeviceJSON do
     device
     |> Map.take([:id, :name, :type, :state, :timezone, :inserted_at, :updated_at])
     |> Map.put(:stream_config, Map.from_struct(device.stream_config))
-    |> Map.put(:credentials, Map.from_struct(device.credentials))
   end
 end
