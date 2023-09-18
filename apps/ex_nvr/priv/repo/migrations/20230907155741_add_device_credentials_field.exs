b0f500cf3899cfa204ce05deb6bb549d7f9c4a21
Mebareksaf <mebareksafari8@gmail.com>
2023-09-18 12:31:48 +0100

    use a raw sql statements to migrate existing data to new schema

--- apps/ex_nvr/priv/repo/migrations/20230907155741_add_device_credentials_field.exs
+++
@@ -1,9 +0,0 @@
-defmodule ExNVR.Repo.Migrations.AddDeviceCredentialsField do
-  use Ecto.Migration
-
-  def change do
-    alter table(:devices) do
-      add :credentials, :map
-    end
-  end
-end
