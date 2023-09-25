b858819c1dd34bc5234cb68a951efb77b3f8e1ef
Mebarek Saf <30500856+Mebareksaf@users.noreply.github.com>
2023-09-25 13:45:30 +0100

    Recordings tests (#167)
    * fix pagination on filters and add stylling to no results
    
    * fixed styling for No results in recordings filter
    
    * add new moduletag devices with the nbr of devices desirable
    
    * Added tests for filtering and sorting recordings
    
    * assuring recordings are rendered and others not in filtering + enhancing sorting tests
    
    * fix test name
    
    * separate use and imports
    
    * revert deleting spaces

--- apps/ex_nvr_web/lib/ex_nvr_web/live/recordings_list_live.ex
+++ apps/ex_nvr_web/lib/ex_nvr_web/live/recordings_list_live.ex
@@ -113,6 +113,29 @@ defmodule ExNVRWeb.RecordingListLive do
               </svg>
             </a>
           </li>
+          <%= if @meta.total_pages == 0 do %>
+            <li>
+              <a
+                href="#"
+                phx-click="nav"
+                phx-value-page={0}
+                class={
+                  [
+                    "flex items-center justify-center px-3 h-8 leading-tight border dark:border-gray-700"
+                  ] ++
+                    if @meta.current_page == 0,
+                      do: [
+                        "z-10 pointer-events-none text-blue-600 bg-blue-50 border-blue-300 hover:bg-blue-100 hover:text-blue-700 dark:bg-gray-700 dark:text-white"
+                      ],
+                      else: [
+                        "text-gray-500 bg-white border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+                      ]
+                }
+              >
+                <%= 0 %>
+              </a>
+            </li>
+          <% end %>
           <%= if @meta.total_pages > 6 do %>
             <%= for page <- [1,2] do %>
               <li>
--- apps/ex_nvr_web/test/ex_nvr_web/live/recordings_list_live_test.exs
+++ apps/ex_nvr_web/test/ex_nvr_web/live/recordings_list_live_test.exs
@@ -2,11 +2,22 @@ defmodule ExNVRWeb.RecordingListLiveTest do
   @moduledoc false
   use ExNVRWeb.ConnCase
 
-  import ExNVR.{AccountsFixtures, RecordingsFixtures}
+  import ExNVR.{AccountsFixtures, RecordingsFixtures, DevicesFixtures}
   import Phoenix.LiveViewTest
 
   @moduletag :tmp_dir
   @moduletag :device
+
+  defp create_device_directories(device) do
+    File.mkdir!(ExNVR.Utils.recording_dir(device.id))
+    File.mkdir!(ExNVR.Utils.bif_dir(device.id))
+  end
+
+  defp refute_recording_info(lv, recording) do
+    refute lv
+           |> element(~s{[id="recording-#{recording.id}-link"]})
+           |> has_element?()
+  end
 
   defp assert_recording_info(lv, html, device, recording) do
     assert html =~ "#{recording.id}"
@@ -32,11 +43,23 @@ defmodule ExNVRWeb.RecordingListLiveTest do
 
   describe "Recording list page" do
     setup %{device: device} do
+      new_device = device_fixture(%{name: "Device_yxz"})
+      create_device_directories(new_device)
+
       %{
         recordings:
           Enum.map(1..10, fn idx ->
             recording_fixture(device,
-              start_date: DateTime.add(~U(2023-09-12 00:00:00Z), idx * 100)
+              start_date: DateTime.add(~U(2023-09-12 00:00:00Z), idx * 100),
+              end_date: DateTime.add(~U(2023-09-14 00:00:00Z), idx * 100)
+            )
+          end),
+        new_device: new_device,
+        new_recordings:
+          Enum.map(1..5, fn idx ->
+            recording_fixture(new_device,
+              start_date: DateTime.add(~U(2023-09-01 00:00:00Z), idx * 100),
+              end_date: DateTime.add(~U(2024-09-01 00:00:00Z), idx * 100)
             )
           end)
       }
@@ -102,6 +125,150 @@ defmodule ExNVRWeb.RecordingListLiveTest do
 
       assert {:redirect, %{to: path}} = redirect
       assert path == "/api/devices/#{recording.device_id}/recordings/#{recording.filename}/blob"
+    end
+
+    test "Filter recordings by device", %{
+      conn: conn,
+      recordings: recordings,
+      new_device: new_device,
+      new_recordings: new_recordings
+    } do
+      {:ok, lv, _html} =
+        conn
+        |> log_in_user(user_fixture())
+        |> live(~p"/recordings")
+
+      result =
+        lv
+        |> form("#recording-filter-form", %{
+          "filters[0][value]" => "#{new_device.id}"
+        })
+        |> render_change()
+
+      assert result =~ "1"
+
+      for new_recording <- new_recordings,
+          do: assert_recording_info(lv, result, new_device, new_recording)
+
+      for recording <- recordings,
+          do: refute_recording_info(lv, recording)
+    end
+
+    test "Filter recordings by device with no recordings", %{conn: conn} do
+      new_device = device_fixture()
+      create_device_directories(new_device)
+
+      {:ok, lv, _html} =
+        conn
+        |> log_in_user(user_fixture())
+        |> live(~p"/recordings")
+
+      result =
+        lv
+        |> form("#recording-filter-form", %{
+          "filters[0][value]" => "#{new_device.id}"
+        })
+        |> render_change()
+
+      assert result =~ "0"
+      assert result =~ "No results."
+    end
+
+    test "Filter recordings by start date and end date", %{
+      conn: conn,
+      new_recordings: new_recordings,
+      recordings: recordings,
+      device: device
+    } do
+      logged_in_conn =
+        conn
+        |> log_in_user(user_fixture())
+
+      {:ok, lv, _html} =
+        logged_in_conn
+        |> live(~p"/recordings")
+
+      result =
+        lv
+        |> form("#recording-filter-form", %{
+          "filters[1][value]" => ~U(2023-09-12 00:00:00Z)
+        })
+        |> render_change()
+
+      for recording <- recordings, do: assert_recording_info(lv, result, device, recording)
+      for new_recording <- new_recordings, do: refute_recording_info(lv, new_recording)
+
+      # Filter by End Date
+      {:ok, lv, _html} =
+        logged_in_conn
+        |> live(~p"/recordings")
+
+      result =
+        lv
+        |> form("#recording-filter-form", %{
+          "filters[2][value]" => ~U(2023-09-16 00:00:00Z)
+        })
+        |> render_change()
+
+      for recording <- recordings, do: assert_recording_info(lv, result, device, recording)
+      for new_recording <- new_recordings, do: refute_recording_info(lv, new_recording)
+    end
+
+    test "Filter recordings by start date and end date (No results)", %{conn: conn} do
+      {:ok, lv, _html} =
+        conn
+        |> log_in_user(user_fixture())
+        |> live(~p"/recordings")
+
+      result =
+        lv
+        |> form("#recording-filter-form", %{
+          "filters[1][value]" => ~U(2150-09-12 00:00:00Z),
+          "filters[2][value]" => ~U(2023-09-11 00:00:00Z)
+        })
+        |> render_change()
+
+      assert result =~ "phx-click=\"nav\" phx-value-page=\"0\" "
+      assert result =~ "No results."
+    end
+
+    test "Sort recordings by Device Name, Start Date and End Date", %{
+      conn: conn,
+      device: device,
+      recordings: recordings,
+      new_device: new_device,
+      new_recordings: new_recordings
+    } do
+      logged_in_conn =
+        conn
+        |> log_in_user(user_fixture())
+
+      # Sort by Device Name ASC
+      {:ok, lv, html} =
+        logged_in_conn
+        |> live(
+          ~p"/recordings?page_size=1&order_by[]=device_name&order_by[]=start_date&order_by[]=end_date&order_directions[]=asc&order_directions[]=desc"
+        )
+
+      assert_recording_info(lv, html, device, List.last(recordings))
+
+      # Sort by Start date ASC
+      {:ok, lv, html} =
+        logged_in_conn
+        |> live(
+          ~p"/recordings?page_size=1&order_by[]=start_date&order_by[]=device_name&order_directions[]=asc&order_directions[]=asc"
+        )
+
+      assert_recording_info(lv, html, new_device, List.first(new_recordings))
+
+      # Sort by End date DESC
+      {:ok, lv, html} =
+        logged_in_conn
+        |> live(
+          ~p"/recordings?page_size=1&order_by[]=end_date&order_by[]=device_name&order_directions[]=desc&order_directions[]=desc"
+        )
+
+      assert_recording_info(lv, html, new_device, List.last(new_recordings))
     end
   end
 end
