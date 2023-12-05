ee633d4f817b81c12e46c0e432cfcfc2d2fcd4d1
Mebareksaf <mebareksafari8@gmail.com>
2023-12-05 19:51:56 +0100

    adapt device_controller to Authorization Plug

--- apps/ex_nvr_web/lib/ex_nvr_web/controllers/api/device_controller.ex
+++ apps/ex_nvr_web/lib/ex_nvr_web/controllers/api/device_controller.ex
@@ -5,7 +5,7 @@ defmodule ExNVRWeb.API.DeviceController do
 
   action_fallback ExNVRWeb.API.FallbackController
 
-  plug ExNVRWeb.Plug.Authorize, resource: ExNVR.Model.Device
+  plug ExNVRWeb.Plug.Authorize, resource: :device
   plug ExNVRWeb.Plug.Device, [field_name: "id"] when action in [:update, :show]
 
   alias ExNVR.{Devices, DeviceSupervisor}
