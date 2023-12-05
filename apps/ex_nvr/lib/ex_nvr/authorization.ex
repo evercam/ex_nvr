c2b8ad0a07bfdd72e38baa8dca8f2ab80d51cc59
Mebareksaf <mebareksafari8@gmail.com>
2023-12-05 19:42:40 +0100

    rewrite authorization module rules and permissions

--- apps/ex_nvr/lib/ex_nvr/authorization.ex
+++ apps/ex_nvr/lib/ex_nvr/authorization.ex
@@ -1,65 +1,10 @@
 defmodule ExNVR.Authorization do
-  alias __MODULE__
-  alias ExNVR.Model.{Device, Recording}
+  @moduledoc """
+  Basic user authorization module by role.
+  """
+  def is_authorized?(:admin, :device, _action), do: true
+  def is_authorized?(:user, :device, :read), do: true
+  def is_authorized?(_role, :recording, :read), do: true
 
-  defstruct role: nil,
-            create: %{},
-            read: %{},
-            update: %{},
-            delete: %{}
-
-  def can(:admin = role) do
-    grant(role)
-    |> all(Device)
-    |> all(Recording)
-  end
-
-  def can(:user = role) do
-    grant(role)
-    |> read(Device)
-    |> read(Recording)
-  end
-
-  def grant(role), do: %Authorization{role: role}
-
-  def read(authorization, resource), do: put_action(authorization, :read, resource)
-
-  def create(authorization, resource), do: put_action(authorization, :create, resource)
-
-  def update(authorization, resource), do: put_action(authorization, :update, resource)
-
-  def delete(authorization, resource), do: put_action(authorization, :delete, resource)
-
-  def all(authorization, resource) do
-    authorization
-    |> read(resource)
-    |> create(resource)
-    |> update(resource)
-    |> delete(resource)
-  end
-
-  def read?(authorization, resource) do
-    Map.get(authorization.read, resource, false)
-  end
-
-  def create?(authorization, resource) do
-    Map.get(authorization.create, resource, false)
-  end
-
-  def update?(authorization, resource) do
-    Map.get(authorization.update, resource, false)
-  end
-
-  def delete?(authorization, resource) do
-    Map.get(authorization.delete, resource, false)
-  end
-
-  defp put_action(authorization, action, resource) do
-    updated_action =
-      authorization
-      |> Map.get(action)
-      |> Map.put(resource, true)
-
-    Map.put(authorization, action, updated_action)
-  end
+  def is_authorized?(_role, _resource, _action), do: false
 end
