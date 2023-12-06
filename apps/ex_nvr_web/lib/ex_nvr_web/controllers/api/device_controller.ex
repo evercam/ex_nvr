042d323dc26bf63bb84ee6ce4c589e7aba53c459
Mebareksaf <mebareksafari8@gmail.com>
2023-12-07 00:21:37 +0100

    remove Authorization plug and use validation on the functions

--- apps/ex_nvr_web/lib/ex_nvr_web/controllers/api/device_controller.ex
+++ apps/ex_nvr_web/lib/ex_nvr_web/controllers/api/device_controller.ex
@@ -5,8 +5,10 @@ defmodule ExNVRWeb.API.DeviceController do
 
   action_fallback ExNVRWeb.API.FallbackController
 
-  plug ExNVRWeb.Plug.Authorize, resource: :device
   plug ExNVRWeb.Plug.Device, [field_name: "id"] when action in [:update, :show]
+
+  import ExNVR.Authorization
+  import ExNVRWeb.Controller.Helpers
 
   alias ExNVR.{Devices, DeviceSupervisor}
   alias ExNVR.Model.Device
@@ -14,7 +16,10 @@ defmodule ExNVRWeb.API.DeviceController do
 
   @spec create(Conn.t(), map()) :: Conn.t() | {:error, Ecto.Changeset.t()}
   def create(%Conn{} = conn, params) do
-    with {:ok, device} <- Devices.create(params) do
+    user = conn.assigns.current_user
+
+    with :ok <- authorized?(user, :device, :create),
+         {:ok, device} <- Devices.create(params) do
       if Device.recording?(device) do
         DeviceSupervisor.start(device)
       end
@@ -22,14 +27,18 @@ defmodule ExNVRWeb.API.DeviceController do
       conn
       |> put_status(201)
       |> render(:show, device: device)
+    else
+      unauthorized(conn)
     end
   end
 
   @spec update(Conn.t(), map()) :: Conn.t() | {:error, Ecto.Changeset.t()}
   def update(%Conn{} = conn, params) do
     device = conn.assigns.device
+    user = conn.assigns.current_user
 
-    with {:ok, updated_device} <- Devices.update(device, params) do
+    with :ok <- authorized?(user, :device, :update),
+         {:ok, updated_device} <- Devices.update(device, params) do
       cond do
         device.state != updated_device.state and not Device.recording?(updated_device.state) ->
           DeviceSupervisor.stop(updated_device)
@@ -45,16 +54,26 @@ defmodule ExNVRWeb.API.DeviceController do
       end
 
       render(conn, :show, device: device)
+    else
+      unauthorized(conn)
     end
   end
 
   @spec index(Conn.t(), map()) :: Conn.t() | {:error, Ecto.Changeset.t()}
   def index(%Conn{} = conn, params) do
-    render(conn, :list, devices: Devices.list(params))
+    user = conn.assigns.current_user
+
+    if :ok = authorized?(user, :device, :read),
+      do: render(conn, :list, devices: Devices.list(params)),
+      else: unauthorized(conn)
   end
 
   @spec show(Conn.t(), map()) :: Conn.t() | {:error, Ecto.Changeset.t()}
   def show(%Conn{} = conn, _params) do
-    render(conn, :show, device: conn.assigns.device)
+    user = conn.assigns.current_user
+
+    if :ok = authorized?(user, :device, :read),
+      do: render(conn, :show, device: conn.assigns.device),
+      else: unauthorized(conn)
   end
 end
