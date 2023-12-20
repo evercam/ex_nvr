3595e73adfc25fd8871ab9f5580d9b98fbad1b48
Mebareksaf <mebareksafari8@gmail.com>
2023-12-20 10:55:22 +0100

    add playback speed in device streaming controller

--- apps/ex_nvr_web/lib/ex_nvr_web/controllers/api/device_streaming_controller.ex
+++ apps/ex_nvr_web/lib/ex_nvr_web/controllers/api/device_streaming_controller.ex
@@ -21,7 +21,6 @@ defmodule ExNVRWeb.API.DeviceStreamingController do
       query_params = [stream_id: Utils.generate_token(), live: is_nil(params.pos)]
       path = start_hls_pipeline(conn.assigns.device, params, query_params[:stream_id])
       manifest_file = File.read!(Path.join(path, "index.m3u8"))
-
       conn
       |> put_resp_content_type("application/vnd.apple.mpegurl")
       |> send_resp(
@@ -173,12 +172,13 @@ defmodule ExNVRWeb.API.DeviceStreamingController do
   end
 
   defp validate_hls_stream_params(params) do
-    types = %{pos: :utc_datetime, stream: :integer, resolution: :integer}
+    types = %{pos: :utc_datetime, stream: :integer, resolution: :integer, playback_speed: :float}
 
-    {%{pos: nil, stream: nil, resolution: nil}, types}
+    {%{pos: nil, stream: nil, resolution: nil, playback_speed: nil}, types}
     |> Changeset.cast(params, Map.keys(types))
     |> Changeset.validate_inclusion(:stream, [0, 1])
     |> Changeset.validate_inclusion(:resolution, [240, 480, 640, 720, 1080])
+    |> Changeset.validate_number(:playback_speed, greater_than: 0, less_than: 6)
     |> Changeset.apply_action(:create)
   end
 
@@ -264,7 +264,8 @@ defmodule ExNVRWeb.API.DeviceStreamingController do
       start_date: params.pos,
       resolution: params.resolution,
       directory: path,
-      segment_name_prefix: UUID.uuid4()
+      segment_name_prefix: UUID.uuid4(),
+      playback_speed: params.playback_speed
     ]
 
     {:ok, _, pid} = HlsPlayback.start(pipeline_options)
