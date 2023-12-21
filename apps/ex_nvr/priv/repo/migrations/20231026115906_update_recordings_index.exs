a455512d8e565414fe1661bdb8de818847e57ab2
Billal GHILAS <bilal@evercam.io>
2023-12-21 15:07:29 +0100

    delete unncessary migration script

--- apps/ex_nvr/priv/repo/migrations/20231026115906_update_recordings_index.exs
+++
@@ -1,8 +0,0 @@
-defmodule ExNVR.Repo.Migrations.UpdateRecordingsIndex do
-  use Ecto.Migration
-
-  def change do
-    drop index("recordings", [:filename], unique: true)
-    create index("recordings", [:device_id, :filename], unique: true)
-  end
-end
