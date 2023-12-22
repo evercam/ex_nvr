c299e53d9426958db337297e77f59721c1317c4d
Mebareksaf <mebareksafari8@gmail.com>
2023-12-22 21:56:55 +0100

    add :user resource rules in authorization

--- apps/ex_nvr/lib/ex_nvr/authorization.ex
+++ apps/ex_nvr/lib/ex_nvr/authorization.ex
@@ -4,6 +4,7 @@ defmodule ExNVR.Authorization do
   """
   alias ExNVR.Accounts.User
   def authorize(%User{role: :admin}, _resource, _action), do: :ok
+  def authorize(%User{role: :user}, :user, _action), do: {:error, :unauthorized}
   def authorize(%User{role: :user}, _resource, :read), do: :ok
   def authorize(_user, _resource, _action), do: {:error, :unauthorized}
 end
