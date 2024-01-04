a43ff2bbc0879afe085b14a8b2f4fd65625f4326
Mebareksaf <youcefsafari@gmail.com>
2024-01-04 15:46:29 +0100

    integrate authorization check as a local plug

--- apps/ex_nvr_web/lib/ex_nvr_web/controllers/api/user_controller.ex
+++ apps/ex_nvr_web/lib/ex_nvr_web/controllers/api/user_controller.ex
@@ -1,3 +1,59 @@
+defmodule ExNVRWeb.Plug.User do
+  @moduledoc """
+  Loads the user instance from database
+  """
+
+  import ExNVRWeb.Controller.Helpers
+
+  require Logger
+
+  alias ExNVR.Accounts
+  alias Plug.Conn
+
+  def init(opts), do: opts
+
+  def call(%Conn{} = conn, opts) do
+    field_name = Keyword.get(opts, :field_name, "user_id")
+    user_id = conn.path_params[field_name]
+
+    case Accounts.get_user(user_id) do
+      %Accounts.User{} = user ->
+        Logger.metadata(user_id: user.id)
+        Conn.assign(conn, :user_instance, user)
+
+      nil ->
+        conn
+        |> not_found()
+        |> Conn.halt()
+    end
+  end
+end
+
+defmodule ExNVRWeb.Plug.Authorization do
+  @moduledoc """
+  Check if user is authorized.
+  """
+
+  import ExNVRWeb.Controller.Helpers
+  import ExNVR.Authorization
+
+  alias Plug.Conn
+
+  def init(opts), do: opts
+
+  def call(%Conn{} = conn, _opts) do
+    user = conn.assigns.current_user
+
+    case authorize(user, :user, :any) do
+      :ok -> conn
+      {:error, :unauthorized} ->
+        conn
+        |> unauthorized()
+        |> Conn.halt()
+    end
+  end
+end
+
 defmodule ExNVRWeb.API.UserController do
   @moduledoc false
 
@@ -6,18 +62,14 @@ defmodule ExNVRWeb.API.UserController do
   action_fallback ExNVRWeb.API.FallbackController
 
   plug ExNVRWeb.Plug.User, [field_name: "id"] when action in [:update, :show, :delete]
-
-  import ExNVR.Authorization
+  plug ExNVRWeb.Plug.Authorization
 
   alias ExNVR.Accounts
   alias Plug.Conn
 
   @spec create(Conn.t(), map()) :: Conn.t() | {:error, Ecto.Changeset.t()}
   def create(%Conn{} = conn, params) do
-    user = conn.assigns.current_user
-
-    with :ok <- authorize(user, :user, :create),
-         {:ok, created_user} <- Accounts.register_user(params) do
+    with {:ok, created_user} <- Accounts.register_user(params) do
       conn
       |> put_status(201)
       |> render(:show, user_instance: created_user)
@@ -27,37 +79,27 @@ defmodule ExNVRWeb.API.UserController do
   @spec update(Conn.t(), map()) :: Conn.t() | {:error, Ecto.Changeset.t()}
   def update(%Conn{} = conn, params) do
     user_instance = conn.assigns.user_instance
-    user = conn.assigns.current_user
 
-    with :ok <- authorize(user, :user, :update),
-         {:ok, updated_user} <- Accounts.update_user(user_instance, params) do
+    with {:ok, updated_user} <- Accounts.update_user(user_instance, params) do
       render(conn, :show, user_instance: updated_user)
     end
   end
 
   def delete(%Conn{} = conn, _params) do
     user_instance = conn.assigns.user_instance
-    user = conn.assigns.current_user
 
-    with :ok <- authorize(user, :user, :delete),
-         {:ok, _deleted_user} <- Accounts.delete_user(user_instance) do
+    with {:ok, _deleted_user} <- Accounts.delete_user(user_instance) do
       send_resp(conn, 204, "")
     end
   end
 
   @spec index(Conn.t(), map()) :: Conn.t() | {:error, Ecto.Changeset.t()}
   def index(%Conn{} = conn, _params) do
-    user = conn.assigns.current_user
-
-    with :ok <- authorize(user, :user, :read),
-         do: render(conn, :list, users: Accounts.list())
+    render(conn, :list, users: Accounts.list())
   end
 
   @spec show(Conn.t(), map()) :: Conn.t() | {:error, Ecto.Changeset.t()}
   def show(%Conn{} = conn, _params) do
-    user = conn.assigns.current_user
-
-    with :ok <- authorize(user, :user, :read),
-         do: render(conn, :show, user_instance: conn.assigns.user_instance)
+    render(conn, :show, user_instance: conn.assigns.user_instance)
   end
 end
