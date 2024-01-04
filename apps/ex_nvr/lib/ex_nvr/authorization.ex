2f45231f6499e4cf9980ed7cf4a33450eef5f24a
Mebareksaf <youcefsafari@gmail.com>
2024-01-04 14:04:07 +0100

    add a blank space

--- apps/ex_nvr/lib/ex_nvr/authorization.ex
+++ apps/ex_nvr/lib/ex_nvr/authorization.ex
@@ -3,6 +3,7 @@ defmodule ExNVR.Authorization do
   Basic user authorization module by role.
   """
   alias ExNVR.Accounts.User
+
   def authorize(%User{role: :admin}, _resource, _action), do: :ok
   def authorize(%User{role: :user}, :user, _action), do: {:error, :unauthorized}
   def authorize(%User{role: :user}, _resource, :read), do: :ok
