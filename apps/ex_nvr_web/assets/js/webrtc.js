0ed3a0d78d77e5c57f406f86e393c464d35f4857
walid-salah <walid.salah@evercam.io>
2024-01-05 16:14:40 +0100

    Export logs to ui for webrtc

--- apps/ex_nvr_web/assets/js/webrtc.js
+++ apps/ex_nvr_web/assets/js/webrtc.js
@@ -1,54 +1,56 @@
 import { Socket } from "phoenix"
 import { WebRTCEndpoint } from "@jellyfish-dev/membrane-webrtc-js"
 
-function getWebSocketEndpoint() {
-    const protocol = window.location.protocol === 'https:' ? 'wss://' : 'ws://'
-    const hostname = window.location.hostname
-    const port = window.location.port ? `:${window.location.port}` : ''
-    
-    return `${protocol}${hostname}${port}`
-}
-
 window.onload = function (_event) {
     const player = document.getElementById("webRtcPlayer")
+    const logsComponent = document.getElementById("webRtcLogs")
     const deviceId = player.dataset.device
     const webrtc = new WebRTCEndpoint()
     
-    const socket = new Socket(getWebSocketEndpoint() + "/socket", {params: {}})
+    const socket = new Socket("/socket", {params: {}})
     const channel = socket.channel(`device:${deviceId}`)
 
+    function log(message) {
+        if (!logsComponent) {
+            return
+        }
+
+        logsComponent.innerHTML += "\n" + message
+        logsComponent.scrollTop = logsComponent.scrollHeight
+    }
+
     channel.on("media_event", ({ data }) => {
-        console.log("Received event from channel: ", data)
+        log("Received event from channel: \n", data)
         webrtc.receiveMediaEvent(data)
     })
 
     channel.join()
         .receive("ok", resp => {
-            console.log("Joined successfully: ", resp)
+            log("Joined successfully: ", resp)
 
             // WebRTC
             webrtc.on("connected", (endpoint_id) => {
-                console.log("Peer connected successfully: ", endpoint_id)
+                log("Peer connected successfully: \n" + endpoint_id)
             })
 
             webrtc.on("connectionError", (message) => {
-                console.log("Unable to connect peer: ", message)
+                log("Unable to connect peer: \n" + message)
             })
             
             webrtc.on("sendMediaEvent", event => {
-                console.log("Peer will send media event: ", event)
+                log("Peer will send media event: \n" + event)
                 channel.push("media_event", event)
             })
 
             webrtc.on("trackReady", (track_context) => {
-                console.log("Track is ready: ", track_context.stream)
+                log("Track is ready: \n" + track_context.stream)
                 // let video = document.createElement("video")
                 player.srcObject = track_context.stream
             })
 
             webrtc.connect({displayName: "webrtc"})
         })
-        .receive("error", resp => {console.log("Unable to join: ", resp)})
+        .receive("error", resp => {log("Unable to join: \n" + resp)})
 
     socket.connect()
 }
