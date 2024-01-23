de7762867614872139316fcb5a557fca0d852498
Billal GHILAS <bilal@evercam.io>
2024-01-23 12:40:20 +0100

    Fetch available disks

---
+++ apps/ex_nvr/lib/ex_nvr/system_information/disk.ex
@@ -0,0 +1,30 @@
+defmodule ExNVR.SystemInformation.Disk do
+  @moduledoc """
+  A module representing a storage device: HDD, SSD, memory card, ...etc.
+  """
+
+  use Ecto.Schema
+
+  alias Ecto.Changeset
+
+  @type t :: %__MODULE__{}
+
+  schema "storage_devices" do
+    field :vendor, :string
+    field :model, :string
+    field :serial, :string
+    field :type, :string
+    field :size, :integer
+    field :transport, :string
+    field :hotplug, :boolean, default: false
+
+    timestamps(type: :utc_datetime_usec)
+  end
+
+  @spec changeset(map()) :: Changeset.t()
+  def changeset(params) do
+    %__MODULE__{}
+    |> Changeset.cast(params, __MODULE__.__schema__(:fields))
+    |> Changeset.validate_required([:model, :serial, :size])
+  end
+end
---
+++ apps/ex_nvr/lib/ex_nvr/system_information/disks.ex
@@ -0,0 +1,124 @@
+defmodule ExNVR.SystemInformation.Disks do
+  @moduledoc """
+  A module to get available disks on the machine using external utilities.
+
+  Currently only `linux` machines are supported with the use of [lsblk](https://man7.org/linux/man-pages/man8/lsblk.8.html).
+  """
+
+  @manufacturers [
+    {~r/WESTERN.*/, "Western Digital"},
+    {~r/^WDC.*/, "Western Digital"},
+    {~r/"WD.*/, "Western Digital"},
+    {~r/"TOSHIBA./, "Toshiba"},
+    {~r/"HITACHI./, "Hitachi"},
+    {~r/^IC./, "Hitachi"},
+    {~r/^HTS./, "Hitachi"},
+    {~r/SANDISK./, "SanDisk"},
+    {~r/KINGSTON./, "Kingston Technology"},
+    {~r/^SONY./, "Sony"},
+    {~r/TRANSCEND./, "Transcend"},
+    {~r/SAMSUNG./, "Samsung"},
+    {~r/^ST(?!I\\ )./, "Seagate"},
+    {~r/^STI\\ ./, "SimpleTech"},
+    {~r/^D...-./, "IBM"},
+    {~r/^IBM./, "IBM"},
+    {~r/^FUJITSU./, "Fujitsu"},
+    {~r/^MP./, "Fujitsu"},
+    {~r/^MK./, "Toshiba"},
+    {~r/MAXTO./, "Maxtor"},
+    {~r/PIONEER./, "Pioneer"},
+    {~r/PHILIPS./, "Philips"},
+    {~r/QUANTUM./, "Quantum Technology"},
+    {~r/FIREBALL./, "Quantum Technology"},
+    {~r/^VBOX./, "VirtualBox"},
+    {~r/CORSAIR./, "Corsair Components"},
+    {~r/CRUCIAL./, "Crucial"},
+    {~r/ECM./, "ECM"},
+    {~r/INTEL./, "INTEL"},
+    {~r/EVO./, "Samsung"},
+    {~r/APPLE./, "Apple"}
+  ]
+
+  require Logger
+
+  alias ExNVR.SystemInformation.Disk
+
+  @spec list(binary() | nil) :: [Disk.t()]
+  def list(mountpoint \\ nil) do
+    case :os.type() do
+      {:unix, :linux} ->
+        list_linux_disks(mountpoint)
+
+      _other ->
+        []
+    end
+  end
+
+  defp list_linux_disks(mountpoint) do
+    with {:ok, data} <- run_lsblk_cmd(),
+         {:ok, data} <- Jason.decode(data) do
+      data
+      |> Map.get("blockdevices", [])
+      |> Enum.reject(&String.match?(&1["name"], ~r/^(loop|ram)/))
+      |> maybe_filter_by_mountpoint(mountpoint)
+      |> Enum.map(&map_linux_device_to_disk/1)
+    else
+      {:error, reason} = error ->
+        Logger.error(inspect(reason))
+        error
+    end
+  end
+
+  defp run_lsblk_cmd() do
+    case System.cmd("lsblk", ["-bJO"], env: [{"LC_ALL", "C"}], stderr_to_stdout: true) do
+      {data, 0} ->
+        {:ok, data}
+
+      {error, exit_status} ->
+        error_msg = """
+        lsblk failed with status: #{exit_status}
+        #{inspect(error)}
+        """
+
+        {:error, error_msg}
+    end
+  end
+
+  defp maybe_filter_by_mountpoint(devices, nil), do: devices
+
+  defp maybe_filter_by_mountpoint(devices, mountpoint) do
+    Enum.filter(devices, fn device ->
+      device["mountpoint"] == mountpoint or
+        Enum.any?(device["children"] || [], &(&1["mountpoint"] == mountpoint))
+    end)
+  end
+
+  defp map_linux_device_to_disk(device) do
+    %Disk{
+      vendor: get_vendor(device),
+      model: device["model"],
+      serial: device["serial"],
+      size: device["size"],
+      type: get_disk_type(device),
+      transport: device["tran"],
+      hotplug: device["hotplug"]
+    }
+  end
+
+  defp get_vendor(device) do
+    model = String.upcase(device["model"])
+    default_value = String.trim("#{device["vendor"]}")
+
+    Enum.find_value(@manufacturers, default_value, fn {pattern, manufacturer} ->
+      if String.match?(model, pattern), do: manufacturer
+    end)
+  end
+
+  defp get_disk_type(device) do
+    cond do
+      device["rota"] -> "HDD"
+      device["tran"] == "nvme" -> "NVMe"
+      true -> "SSD"
+    end
+  end
+end
