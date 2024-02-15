589d4afdc479e0e43cde035a9ad152a28ce360ef
Billal GHILAS <bilal@evercam.io>
2024-02-15 10:41:08 +0100

    Add tests

--- apps/ex_nvr/lib/ex_nvr/remote_storages/store/http.ex
+++ apps/ex_nvr/lib/ex_nvr/remote_storages/store/http.ex
@@ -19,11 +19,6 @@ defmodule ExNVR.RemoteStorages.Store.HTTP do
   def save_recording(device, recording, opts) do
     recording_path = Recordings.recording_path(device, recording)
 
-    json_part =
-      %{device_id: device.id, start_date: recording.start_date}
-      |> Jason.encode!()
-      |> Multipart.Part.text_field("metadata", [{"Content-Type", "application/json"}])
-
     file_part =
       Multipart.Part.file_field(recording_path, "file", [],
         filename: Path.basename(recording_path)
@@ -31,29 +26,27 @@ defmodule ExNVR.RemoteStorages.Store.HTTP do
 
     multipart =
       Multipart.new()
-      |> Multipart.add_part(json_part)
+      |> Multipart.add_part(json_part(%{device_id: device.id, start_date: recording.start_date}))
       |> Multipart.add_part(file_part)
-
-    content_length = Multipart.content_length(multipart)
-    content_type = Multipart.content_type(multipart, "multipart/form-data")
-
-    headers = [
-      {"Content-Length", to_string(content_length)},
-      {"Content-Type", content_type}
-    ]
 
     resp =
       Req.post(opts[:url],
         auth: auth_from_opts(opts),
-        headers: headers,
+        headers: headers_from_multipart(multipart),
         body: Multipart.body_stream(multipart)
       )
 
     case resp do
-      {:ok, %{state: 200}} -> :ok
+      {:ok, %{status: status}} when status >= 200 and status < 300 -> :ok
       {:ok, resp} -> {:error, resp}
       error -> error
     end
+  end
+
+  defp json_part(data) do
+    data
+    |> Jason.encode!()
+    |> Multipart.Part.text_field("metadata", [{"Content-Type", "application/json"}])
   end
 
   defp auth_from_opts(opts) do
@@ -62,5 +55,15 @@ defmodule ExNVR.RemoteStorages.Store.HTTP do
       :basic -> {:basic, "#{opts[:username]}:#{opts[:password]}"}
       _other -> nil
     end
+  end
+
+  defp headers_from_multipart(multipart) do
+    content_length = Multipart.content_length(multipart)
+    content_type = Multipart.content_type(multipart, "multipart/form-data")
+
+    [
+      {"Content-Length", to_string(content_length)},
+      {"Content-Type", content_type}
+    ]
   end
 end
