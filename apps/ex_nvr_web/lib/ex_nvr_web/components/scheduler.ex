4f13cf36f66a21e61a2daeb8ce777e8f2b61aeb0
Mebareksaf <youcefsafari@gmail.com>
2024-03-12 02:48:24 +0100

    use core components instead of raw html

--- apps/ex_nvr_web/lib/ex_nvr_web/components/scheduler.ex
+++ apps/ex_nvr_web/lib/ex_nvr_web/components/scheduler.ex
@@ -4,6 +4,7 @@ defmodule ExNVRWeb.SchedulerComponent do
   """
 
   use Phoenix.LiveComponent
+  import ExNVRWeb.CoreComponents
 
   @impl true
   def render(assigns) do
@@ -16,14 +17,14 @@ defmodule ExNVRWeb.SchedulerComponent do
         <!-- Schedule grid -->
         <div id="scheduler-grid" phx-hook="Scheduler" class="grid grid-cols-8 text-sm">
           <!-- Header -->
-          <div class="bg-blue-500 text-black dark:bg-gray-400 dark:text-white font-bold p-2 border border-black dark:border-white">Time/Day</div>
+          <div class="bg-red-700 text-white dark:bg-gray-400 dark:text-white font-bold p-2 border border-black dark:border-white">Time/Day</div>
           <%= for day <- ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"] do %>
-            <div class="bg-blue-400 text-black dark:bg-gray-500 dark:text-white font-bold p-2 border border-black dark:border-white"><%= day %></div>
+            <div class="bg-red-400 text-black dark:bg-gray-500 dark:text-white font-bold p-2 border border-black dark:border-white"><%= day %></div>
           <% end %>
 
           <!-- Times and Days -->
           <%= for hour <- 0..24//2 do %>
-            <div class="p-2 border border-black dark:border-white bg-blue-400 text-black dark:bg-gray-500 dark:text-white font-bold"> <%= hour %>:00 </div>
+            <div class="p-2 border border-black dark:border-white bg-red-400 text-black dark:bg-gray-500 dark:text-white font-bold"> <%= hour %>:00 </div>
             <%= for day <- ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"] do %>
               <div
                 data-date={day}
@@ -35,29 +36,38 @@ defmodule ExNVRWeb.SchedulerComponent do
         </div>
       </div>
 
-      <div id="task-form" class="hidden absolute bg-white p-4 border border-gray-300 rounded shadow-lg z-10">
-        <form id="task-form-inner">
+      <div id="task-form" class="hidden absolute bg-gray-300 dark:bg-gray-800 p-4 border border-gray-300 rounded shadow-lg z-10 ">
+        <.simple_form id="task-form-inner" for={%{}}>
           <div>
-            <label for="task-name" class="block text-sm font-medium text-gray-700">Task Name</label>
-            <input type="text" id="task-name" name="task_name" placeholder="Enter task name" class="mt-1 block w-full border border-gray-300 rounded-md shadow-sm"/>
+            <.input
+              label="Task Name"
+              type="text"
+              id="task-name"
+              name="task_name"
+              value={}
+              placeholder="Enter task name"
+              class="mt-1 block w-full border border-gray-300 rounded-md shadow-sm"
+              required/>
           </div>
           <div class="mt-4 grid grid-cols-2 gap-4">
             <div>
-              <label for="start-date" class="block text-sm font-medium text-gray-700">Start Date</label>
-              <input
+              <.input
+                label="Start Date"
                 type="select"
                 id="start-date"
                 name="start_date"
+                value={}
                 class="mt-1 block w-full border border-gray-300 rounded-md shadow-sm"
                 options={["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]}
                 />
             </div>
             <div>
-              <label for="end-date" class="block text-sm font-medium text-gray-700">End Date</label>
-              <input
+              <.input
+                label="End Date"
                 type="select"
                 id="end-date"
                 name="end_date"
+                value={}
                 class="mt-1 block w-full border border-gray-300 rounded-md shadow-sm"
                 options={["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]}
                 />
@@ -65,21 +75,23 @@ defmodule ExNVRWeb.SchedulerComponent do
           </div>
           <div class="mt-4 grid grid-cols-2 gap-4">
             <div>
-              <label for="start-time" class="block text-sm font-medium text-gray-700">Start Time</label>
-              <input
+              <.input
+                label="Start Time"
                 type="select"
                 id="start-time"
                 name="start_time"
+                value={}
                 class="mt-1 block w-full border border-gray-300 rounded-md shadow-sm"
                 options={["0:00", "2:00", "4:00", "6:00", "8:00", "10:00", "12:00", "14:00", "16:00", "18:00", "20:00", "22:00"]}
                 />
             </div>
             <div>
-              <label for="end-time" class="block text-sm font-medium text-gray-700">End Time</label>
-              <input
+              <.input
+                label="End Time"
                 type="select"
                 id="end-time"
                 name="end_time"
+                value={}
                 class="mt-1 block w-full border border-gray-300 rounded-md shadow-sm"
                 options={["0:00", "2:00", "4:00", "6:00", "8:00", "10:00", "12:00", "14:00", "16:00", "18:00", "20:00", "22:00"]}
                 />
@@ -89,11 +101,13 @@ defmodule ExNVRWeb.SchedulerComponent do
             <button type="button" id="cancel-task" class="inline-flex justify-center py-2 px-4 mr-4 border border-gray-300 shadow-sm text-sm font-medium rounded-md text-black bg-white hover:bg-gray-100">
               Cancel
             </button>
-            <button type="submit" class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700">
+            <.button
+              class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700"
+              >
               Save Task
-            </button>
+            </.button>
           </div>
-        </form>
+        </.simple_form>
       </div>
     </div>
     """
