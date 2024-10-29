70df9a0e8a94d82a9780a056f23db60889ce1f23
Billal GHILAS <bilal@evercam.io>
2024-10-29 18:56:14 +0100

    update version

--- apps/ex_nvr_web/mix.exs
+++ apps/ex_nvr_web/mix.exs
@@ -4,7 +4,7 @@ defmodule ExNVRWeb.MixProject do
   def project do
     [
       app: :ex_nvr_web,
-      version: "0.15.2",
+      version: "0.15.3",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
