aea24ec80c459e44491a7c1103292a2d636b724b
Halim Bouguedra <halim.bouguedra@gmail.com>
2025-04-19 10:13:31 +0100

    Nerves-preview: Add ability to choose a custom version name

--- .github/workflows/scripts.js
+++ .github/workflows/scripts.js
@@ -2,116 +2,146 @@ const fs = require("fs")
 const path = require("path")
 
 module.exports = {
-  postComment,
-  parseNervesBuildCommand,
-  handleNervesBuildResult,
+    postComment,
+    handleNervesBuildCommand,
+    handleNervesBuildResult,
 }
 
-async function parseNervesBuildCommand({ github, context, core }) {
-  const target = await parseBuildTarget({ github, context, core })
-  const prData = await getPrData({ github, context })
-  const version = await generateCustomVersion(prData, core)
+async function handleNervesBuildCommand({github, context, core}) {
+    const {target, customVersion} = await parseBuildArguments({github, context, core})
+    const prData = await getPrData({github, context})
+    const version = customVersion || await generateCustomVersion({...prData, core})
 
-  core.setOutput("target", target)
-  core.setOutput("version", version)
+    core.setOutput("target", target)
+    core.setOutput("version", version)
 
-  await postComment({
-    github,
-    context,
-    body: `
-      🛠️ Starting preview build for version: **${version}** for target **${
-      formatTarget(target)
-    }**
-    This may take up to 15 minutes. You can check the progress on the [Github Actions page](https://github.com/evercam/ex_nvr/actions) 
-    `,
-  })
-}
-
-async function handleNervesBuildResult({ github, context, version, target, result }) {
-  let body = ""
-  if (result === "success") {
-    body = `
-      ✅ Build succeeded!
-      Version: **${version}** for target: **${formatTarget(target)}** can be found on [NervesHub](https://manage.nervescloud.com/).
-    `
-  } else {
-    body = `
-      ❌ Build failed for version: **${version}**, target: **${formatTarget(target)}**
-      Check the logs for more information.
-    `
-  }
-
-  await postComment({ github, context, body })
-}
-
-async function parseBuildTarget({ github, context, core }) {
-  const comment = context.payload.comment.body
-  const target = comment.match(/^\/build\s+([a-z0-9_]+)/)?.[1]
-  const validTargets = ["ex_nvr_rpi4", "ex_nvr_rpi5", "giraffe"]
-
-  if (target && !validTargets.includes(target)) {
     await postComment({
-      github,
-      context,
-      body: `
-        ❌ Error: Invalid target **${target}** specified.
-        Please try again with one of the following targets, or no target at all:
-        ${validTargets.map((t) => `- ${t}`).join("\n")}
-      `,
+        github,
+        context,
+        body: `
+            🛠️ Starting preview build for version: **${version}** for target **${
+                formatTarget(target)
+            }**
+            This may take up to 15 minutes. You can check the progress on the [Github Actions page](https://github.com/evercam/ex_nvr/actions) 
+        `,
     })
-
-    core.setFailed(`Invalid build target: ${target}`)
-    process.exit()
-    return
-  }
-
-  return target
 }
 
-async function getPrData({ github, context }) {
-  const prUrl = context.payload.issue.pull_request.url
-  const pr = await github.request(`GET ${prUrl}`)
+async function handleNervesBuildResult({github, context, version, target, result}) {
+    let body = ""
+    if (result === "success") {
+        body = `
+            ✅ Build succeeded!
+            Version: **${version}** for target: **${formatTarget(target)}** can be found on [NervesHub](https://manage.nervescloud.com/org/Evercam/ex_nvr_fw/firmware).
+        `
+    } else {
+        body = `
+            ❌ Build failed for version: **${version}**, target: **${formatTarget(target)}**
+            Check the logs for more information.
+        `
+    }
 
-  return {
-    branch: pr.data.head.ref,
-    sha: pr.data.head.sha.substring(0, 7),
-  }
+    await postComment({github, context, body})
 }
 
-async function generateCustomVersion({ branch, sha, core }) {
-  try {
-    const mixExsPath = path.join(
-      process.env.GITHUB_WORKSPACE,
-      "nerves_fw",
-      "mix.exs"
-    )
-    const mixExsContent = fs.readFileSync(mixExsPath, "utf8")
-    const versionRegex = /@version\s+"([^"]+)"/
-    const versionMatch = mixExsContent.match(versionRegex)
+async function parseBuildArguments({github, context, core}) {
+    const comment = context.payload.comment.body
+    const commandRegex = /\/build(?:\s+(?!version=)([^\s]+))?(?:\s+version=([^\s]+))?/
+    const [_, target, customVersion] = commandRegex.exec(comment) || []
 
-    return `${versionMatch[1]}-${branch}-${sha}`
-  } catch (error) {
-    core.setFailed(`Error reading mix.exs: ${error.message}`)
-    process.exit()
-  }
+    await validateTarget({github, context, core, target})
+    await validateVersion({github, context, core, customVersion})
+
+    return {target, customVersion}
 }
 
-async function postComment({ github, context, body }) {
-  const { owner, repo } = context.repo
-  const { number } = context.issue || context.payload.pull_request
+async function validateTarget({github, context, core, target}) {
+    const validTargets = ["ex_nvr_rpi4", "ex_nvr_rpi5", "giraffe"]
+    if (target && !validTargets.includes(target)) {
+        await postComment({
+            github,
+            context,
+            body: `
+                ❌ Error: Invalid target **${target}** specified.
+                Please try again with one of the following targets, or no target at all:
+                ${validTargets.map((t) => `- ${t}`).join("\n")}
+            `,
+        })
 
-  await github.rest.issues.createComment({
-    owner,
-    repo,
-    issue_number: number,
-    body: dedent(body),
-  })
+        core.setFailed(`Invalid build target: ${target}`)
+        process.exit()
+        return {}
+    }
+
+    return true
+}
+
+async function validateVersion({github, context, core, customVersion}) {
+    const semVerRegex = /^\d+\.\d+\.\d+(?:-[\w\d.-]+)?(?:\+[\w\d.-]+)?$/
+    if (customVersion && !semVerRegex.test(customVersion)) {
+        await postComment({
+            github,
+            context,
+            body: `
+                ❌ Error: Invalid version format **${customVersion}**.
+                Versions must follow semantic versioning format: MAJOR.MINOR.PATCH[PRERELEASE][BUILD]
+                Examples: 1.0.0, 1.1.1-abcd, 0.22.1-alpha.1, 0.1.1-test-1, 1.42.3-beta.2+build.123
+            `,
+        })
+
+        core.setFailed(`Invalid version format: ${customVersion}`)
+        process.exit()
+        return
+    }
+
+    return true
+}
+
+async function getPrData({github, context}) {
+    const prUrl = context.payload.issue.pull_request.url
+    const pr = await github.request(`GET ${prUrl}`)
+
+    return {
+        branch: pr.data.head.ref,
+        sha: pr.data.head.sha.substring(0, 6),
+    }
+}
+
+async function generateCustomVersion({branch, sha, core}) {
+    try {
+        const mixExsPath = path.join(
+            process.env.GITHUB_WORKSPACE,
+            "nerves_fw",
+            "mix.exs"
+        )
+        const mixExsContent = fs.readFileSync(mixExsPath, "utf8")
+        const versionRegex = /@version\s+"([^"]+)"/
+        const versionMatch = mixExsContent.match(versionRegex)
+        const formattedBranch = branch.replace(/[^a-zA-Z0-9_-]/g, '').slice(0, 5)
+
+        return `${versionMatch[1]}-${formattedBranch}-${sha}`
+    } catch (error) {
+        core.setFailed(`Error reading mix.exs: ${error.message}`)
+        process.exit()
+    }
+}
+
+async function postComment({github, context, body}) {
+    const {owner, repo} = context.repo
+    const {number} = context.issue || context.payload.pull_request
+
+    await github.rest.issues.createComment({
+        owner,
+        repo,
+        issue_number: number,
+        body: dedent(body),
+    })
 }
 
 function formatTarget(target = "") {
-  return target?.trim()?.length ? target : "all"
+    return target?.trim()?.length ? target : "all"
 }
 
-function dedent (str = "") {
-  return str.split('\n').map(l => l.trim()).join('\n')
+function dedent(str = "") {
+    return str.split('\n').map(l => l.trim()).join('\n')
 }
