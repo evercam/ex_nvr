0c0745383b0de8bdcf1330e2f983b2c5c1b7d840
Billal GHILAS <bilal@evercam.io>
2025-06-18 17:49:36 +0100

    delete comment

--- ui/lib/ex_nvr_web/router.ex
+++ ui/lib/ex_nvr_web/router.ex
@@ -177,12 +177,4 @@ defmodule ExNVRWeb.Router do
       end
     end
   end
-
-  # Kit-Y25T1161 a4EYe1n6
-  # Kit-Y25T1164 Jv41Ykw5
-  # Kit-Y25T1262 B+w4&d3C
-  # Kit-Y25T1258 Wy8k1LMi
-  # Kit-Y25T1251 h0SPp3k4
-  # Kit-Y25T1183 Az7f9K8R
-  # Kit-Y25T1291 9z&V=6Zy
 end
