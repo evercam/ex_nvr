840882aace07577a2fa55ab295667eafb0557206
Billal GHILAS <bilal@evercam.io>
2025-07-14 21:28:53 +0100

    Add file logger and push logs to loki

--- ui/config/runtime.exs
+++ ui/config/runtime.exs
@@ -73,7 +73,7 @@ if config_env() == :prod do
   if log_json? do
     config :logger, :default_handler,
       level: :info,
-      formatter: LoggerJSON.Formatters.Basic.new(metadata: [:request_id])
+      formatter: LoggerJSON.Formatters.Basic.new(metadata: [:device_id, :user_id, :request_id])
   end
 
   config :ex_nvr,
