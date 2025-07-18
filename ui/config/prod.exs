ed0161eb737bcc938c80368a6f9c0a3cf3982468
Billal GHILAS <bilal@evercam.io>
2025-07-18 18:07:39 +0100

    Fix sentry config

--- ui/config/prod.exs
+++ ui/config/prod.exs
@@ -31,7 +31,7 @@ config :live_vue,
 config :sentry,
   release: "ex_nvr@0.22.0",
   report_deps: false,
-  root_source_code_paths: File.cwd!(),
+  root_source_code_paths: [File.cwd!()],
   context_lines: 5,
   environment_name: config_env(),
   enable_source_code_context: true
