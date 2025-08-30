a713b6a678e5ed8ea6b715a23eba58738a0b9c90
Billal Ghilas <84322223+gBillal@users.noreply.github.com>
2025-08-30 12:16:50 +0100

    victron: always retry all serial ports (#787)
    * victron: retry all serial ports
    
    * Bump version
    
    * upgrade deps

--- ui/lib/ex_nvr/hardware/serial_port_checker.ex
+++ ui/lib/ex_nvr/hardware/serial_port_checker.ex
@@ -10,7 +10,8 @@ defmodule ExNVR.Hardware.SerialPortChecker do
 
   require Logger
 
-  @serial_ports_interval_check to_timeout(second: 30)
+  @serial_ports_interval_check to_timeout(minute: 1)
+  @ignore_ports ["ttyS0", "ttyS1", "ttyAMA0", "ttyAMA10"]
 
   def start_link(opts) do
     GenServer.start_link(__MODULE__, opts, name: __MODULE__)
@@ -19,7 +20,7 @@ defmodule ExNVR.Hardware.SerialPortChecker do
   @impl true
   def init(_opts) do
     Process.send_after(self(), :check_serial_ports, 0)
-    {:ok, %{serial_ports: ["ttyS0", "ttyS1", "ttyAMA0", "ttyAMA10"]}}
+    {:ok, nil}
   end
 
   @impl true
@@ -27,18 +28,15 @@ defmodule ExNVR.Hardware.SerialPortChecker do
     Logger.info("Checking for new serial ports")
     Process.send_after(self(), :check_serial_ports, @serial_ports_interval_check)
 
-    serial_ports =
-      Circuits.UART.enumerate()
-      |> Map.keys()
-      |> Kernel.--(state.serial_ports)
-
-    serial_ports
+    Circuits.UART.enumerate()
+    |> Map.keys()
+    |> Kernel.--(@ignore_ports)
     |> Enum.map(&[port: &1, name: :"victron_#{&1}"])
     |> Enum.filter(&is_nil(Process.whereis(&1[:name])))
     |> Enum.each(
       &DynamicSupervisor.start_child(ExNVR.Hardware.Supervisor, {ExNVR.Hardware.Victron, &1})
     )
 
-    {:noreply, %{serial_ports: state.serial_ports ++ serial_ports}}
+    {:noreply, state}
   end
 end
