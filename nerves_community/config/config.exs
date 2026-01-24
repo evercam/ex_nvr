5f87bb3d82cdfbd40cba25f2207c0e27b2f0e35a
Billal GHILAS <bilal@evercam.io>
2026-01-24 18:37:51 +0100

    Add community nerves project

---
+++ nerves_community/config/config.exs
@@ -0,0 +1,20 @@
+import Config
+
+# Enable the Nerves integration with Mix
+Application.start(:nerves_bootstrap)
+
+# Customize non-Elixir parts of the firmware. See
+# https://hexdocs.pm/nerves/advanced-configuration.html for details.
+
+config :nerves, :firmware, rootfs_overlay: "rootfs_overlay"
+
+# Set the SOURCE_DATE_EPOCH date for reproducible builds.
+# See https://reproducible-builds.org/docs/source-date-epoch/ for more information
+
+config :nerves, source_date_epoch: "1758112912"
+
+if Mix.target() == :host do
+  import_config "host.exs"
+else
+  import_config "target.exs"
+end
---
+++ nerves_community/lib/base_fw/application.ex
@@ -0,0 +1,26 @@
+defmodule ExNVR.Nerves.Application do
+  @moduledoc false
+
+  use Application
+
+  @impl true
+  def start(_type, _args) do
+    children =
+      [
+      ] ++ target_children()
+
+    opts = [strategy: :one_for_one, name: ExNVR.Nerves.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+
+  # List all child processes to be supervised
+  if Mix.target() == :host do
+    defp target_children() do
+      []
+    end
+  else
+    defp target_children() do
+      []
+    end
+  end
+end
